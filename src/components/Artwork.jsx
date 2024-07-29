4782f47a8ae7a3969f897f392126d9bf6e5ee688
Fabián Gómez Campo <fgomezc@uxcale.com>
2024-07-29 16:39:13 +0200

    added regex to remove html tags in the response

--- src/components/Artwork.jsx
+++ src/components/Artwork.jsx
@@ -1,7 +1,6 @@
 import { IMG_BASE_URL } from "../utils/constants"
 
 function Artwork({ artwork }) {
-
     return (
         <li className="artwork" key={artwork.id}>
             <h4>{artwork.title}</h4>
@@ -10,7 +9,7 @@ function Artwork({ artwork }) {
             <p>Fecha: {artwork.date_display}</p>
             {artwork.description === null ?
                 <p>No hay descripción</p> :
-                <p>{artwork.description}</p>
+                <p>{artwork.description.replace(/<\/?[^>]+>/gi, '')}</p>
             }
             <img src={`${IMG_BASE_URL}/${artwork.image_id}/full/843,/0/default.jpg`}></img>
         </li>
